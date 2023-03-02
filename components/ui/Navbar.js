84bdac834c3a2747e53ee4753c7a27b3efc2f07d
Alejandro J Roche Benitez <rochebenitez90@gmail.com>
2023-03-02 14:42:14 -0500

    Configuracion de los menu en el tema

--- components/ui/Navbar.js
+++ components/ui/Navbar.js
@@ -1,15 +1,16 @@
 import React, {useState} from "react";
 import NextLink from 'next/link'
 import {AppBar, Box, Button, Container, Link, Toolbar, Typography} from "@mui/material";
+import {UserNavbarMenu} from "./UserNavbarMenu";
 
 export const Navbar = () => {
 
-    const [isAuthenticated, setIsAuthenticated] = useState(false)
+    const [isAuthenticated, setIsAuthenticated] = useState(true)
 
     return (
         <AppBar>
             <Toolbar>
-                <Container style={{display:"flex"}}>
+                <Container style={{display: "flex", alignItems:"center"}}>
                     <NextLink href="/" passHref className="underline-none">
                         <Link component='div' sx={{display: 'flex', alignItems: 'baseline'}}>
                             <Typography variant="h6" fontWeight="700">Blog</Typography>
@@ -20,11 +21,19 @@ export const Navbar = () => {
                     <Box flex={1}/>
 
                     <Box>
-                        <NextLink href={'/auth/login'} passHref>
-                            <Link component={"span"}>
-                                <Button size='small' sx={{borderRadius:'10px', fontWeight:700}}>Login</Button>
-                            </Link>
-                        </NextLink>
+                        {
+                            !isAuthenticated ? (
+                                    <NextLink href={'/auth/login'} passHref>
+                                        <Link component={"span"}>
+                                            <Button size='small' sx={{borderRadius: '10px', fontWeight: 700}}>Login</Button>
+                                        </Link>
+                                    </NextLink>
+                                )
+                                : (
+                                   <UserNavbarMenu />
+                                )
+                        }
+
                     </Box>
 
                 </Container>
--- themes/light.js
+++ themes/light.js
@@ -69,7 +69,29 @@ export const lightTheme = createTheme({
                     boxShadow: "none",
                 }
             }
-        }
+        },
+
+        MuiMenu: {
+            defaultProps: {
+                transformOrigin: {horizontal: 'right', vertical: 'top'},
+                anchorOrigin: {horizontal: 'right', vertical: 'bottom'},
+                PaperProps: {
+                    elevation: 0,
+                    sx: {
+                        borderRadius: 3,
+                        overflow: 'visible',
+                        filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                        mt: 1.5,
+                        '& .MuiAvatar-root': {
+                            width: 32,
+                            height: 32,
+                            ml: -0.5,
+                            mr: 1,
+                        },
+                    },
+                }
+            }
+        },
     }
 
 })
