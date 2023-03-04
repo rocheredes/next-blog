e2836874104b87f1767df4f68212c15a019da619
Alejandro J Roche Benitez <rochebenitez90@gmail.com>
2023-03-03 23:19:17 -0500

    PostSlide Show component

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,41 @@
-import {Button} from "@mui/material";
+import React from "react";
+import {Box, Container} from "@mui/material";
 import {BlogLayout} from "../components/layout/BlogLayout";
+import {PostSlideshow} from "../components/posts/PostSlideshow";
+import Grid from "@mui/material/Grid";
+
+const imageList = [
+    'gallery/images1.jpg',
+    'gallery/images2.jpg',
+    'gallery/images3.jpg',
+    'gallery/images4.jpg',
+    'gallery/images5.jpg',
+]
+
+
 
 export default function Home() {
     return (
         <>
-           <BlogLayout title={"Home"} pageDescription={"Home Page"}>
-               <Button>sdasdasd</Button>
-               <Button>sdasdasd</Button>
-           </BlogLayout>
+            <BlogLayout title={"Home"} pageDescription={"Home Page"}>
+
+                <Container sx={{mt: 2}}>
+                    <Grid container spacing={3}>
+                        <Grid item xs={12} sm={12}>
+                            <PostSlideshow images={imageList}/>
+                        </Grid>
+
+                        <Grid item xs={12} sm={12}>
+                            <PostSlideshow posts={imageList}/>
+                        </Grid>
+
+
+
+                    </Grid>
+                </Container>
+
+
+            </BlogLayout>
         </>
     )
 }
