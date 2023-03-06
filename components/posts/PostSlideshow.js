76775bcb25d53abf3e311a844e3272415a9d3d06
Alejandro J Roche Benitez <rochebenitez90@gmail.com>
2023-03-05 22:38:02 -0500

    Se crea el carousel con react-material-ui-carousel

--- components/posts/PostSlideshow.js
+++ components/posts/PostSlideshow.js
@@ -1,32 +1,65 @@
 import React from "react";
-import {Slide} from "react-slideshow-image";
-import 'react-slideshow-image/dist/styles.css'
-import styles from '../../styles/PostsSlideshow.module.css'
-import {Button} from "@mui/material";
+import Carousel from "react-material-ui-carousel";
+import {Box, CardMedia} from "@mui/material";
 
 export const PostSlideshow = ({images}) => {
     return (
         <>
 
-            <Slide
-                easing="ease"
-            >
+            <Carousel indicators={false}>
                 {
                     images.map(image => {
                         return (
-                            <div className={styles['each-slide']} key={image}
-                                 style={{overflow: "hidden"}}>
-                                <img
+
+                            <Box sx={{
+                                overflow: "hidden",
+
+                            }}>
+                                <CardMedia
+                                    component={"img"}
                                     src={image}
                                     alt={image}
-                                    style={{borderRadius: '5px',}}
+                                    sx={{
+                                        width: "100%",
+                                        borderRadius: '10px',
+                                        height: {xs: "auto", md: '600px'}
+                                    }}
                                 />
-                            </div>
+                            </Box>
+
+                            // <div key={image}
+                            //      style={{overflow: "hidden", borderRadius: '5px'}}>
+                            //     <img
+                            //         src={image}
+                            //         alt={image}
+                            //         style={{borderRadius: '5px'}}
+                            //     />
+                            // </div>
 
                         )
                     })
                 }
-            </Slide>
+            </Carousel>
+
+            {/*<Slide*/}
+            {/*    easing="ease"*/}
+            {/*>*/}
+            {/*    {*/}
+            {/*        images.map(image => {*/}
+            {/*            return (*/}
+            {/*                <div className={styles['each-slide']} key={image}*/}
+            {/*                     style={{overflow: "hidden"}}>*/}
+            {/*                    <img*/}
+            {/*                        src={image}*/}
+            {/*                        alt={image}*/}
+            {/*                        style={{borderRadius: '5px',}}*/}
+            {/*                    />*/}
+            {/*                </div>*/}
+
+            {/*            )*/}
+            {/*        })*/}
+            {/*    }*/}
+            {/*</Slide>*/}
         </>
     )
 }
