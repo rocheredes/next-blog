d5586950ad28aa4a497b2b37f3b94a52e460533d
Alejandro J Roche Benitez <rochebenitez90@gmail.com>
2023-05-01 21:01:22 -0400

    metodos de conexion y desconeccion de la BD

--- database/db.js
+++ database/db.js
@@ -7,3 +7,36 @@ import mongoose from 'mongoose'
  * 2 => connecting
  * 3 => disconnecting
  */
+
+const mongoConnection = {
+    isConnected: 0
+}
+
+export const connect = async () => {
+    if (mongoConnection.isConnected === 1) {
+        return;
+    }
+
+    if (mongoConnection.isConnected > 0) {
+        mongoConnection.isConnected = mongoose.connections[0].readyState
+
+        if (mongoConnection.isConnected === 1) {
+            return;
+        }
+
+        await mongoose.disconnect()
+
+    }
+
+    await mongoose.connect(process.env.MONGO_URL || '')
+    mongoConnection.isConnected = 1
+}
+
+export const disconnect = async () => {
+    if (process.env.NODE_ENV === 'development') return
+
+    if (mongoConnection.isConnected === 0) return
+
+    mongoConnection.isConnected = 0
+    await mongoose.disconnect()
+}
