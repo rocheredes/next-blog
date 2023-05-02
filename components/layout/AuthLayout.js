d4aec041039804c2daf1bec38428ded6923f3d11
Alejandro J Roche Benitez <rochebenitez90@gmail.com>
2023-05-02 19:26:49 -0400

    Auth Layout

---
+++ components/layout/AuthLayout.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import {Box} from "@mui/material";
+
+export const AuthLayout = ({children, title}) => {
+    return (
+        <>
+            <Head>
+                <title>{title}</title>
+            </Head>
+            <main>
+                <Box display="flex" justifyContent="center" alignItems="center" height="calc(100vh - 200px)">
+                    {children}
+                </Box>
+            </main>
+
+
+        </>
+    )
+}
+
+
