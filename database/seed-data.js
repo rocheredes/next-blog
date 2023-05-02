7d9a10894859ab9ca154cfa3ea1187595b718cf9
Alejandro J Roche Benitez <rochebenitez90@gmail.com>
2023-05-01 22:07:22 -0400

    User seed

--- database/seed-data.js
+++ database/seed-data.js
@@ -12,7 +12,8 @@ export const initialData = {
         {
             first_name: 'Alfredo',
             last_name: 'Fonseca Ramirez',
-            username: 'alfredofonseca@gamail.com',
+            username: 'alfredofonseca',
+            email: 'alfredofonseca@gamail.com',
             image_profile: '/gallery/cat-gdf2e404cc_1920.jpg',
             bio: "Louis Ferguson is a senior editor for the blogzine and also reports on breaking news based in London. He has written about government, criminal justice, and the role of money in politics since 2015."
         },
@@ -20,7 +21,8 @@ export const initialData = {
         {
             first_name: 'Pedro',
             last_name: 'Serrano',
-            username: 'pedroserrano@gamail.com',
+            username: 'pedroserrano',
+            email: 'pedroserrano@gamail.com',
             image_profile: '/gallery/cat-gdf2e404cc_1920.jpg',
             bio: "Louis Ferguson is a senior editor for the blogzine and also reports on breaking news based in London. He has written about government, criminal justice, and the role of money in politics since 2015."
         },
--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,7 +1,17 @@
-export default function handler(req, res) {
+import * as db from "../../database/db";
+import User from "../../models/User";
+import {initialData} from "../../database/seed-data";
+
+export default async function handler(req, res) {
     if (process.env.NODE_ENV === 'production') {
         return res.status(401).json({message: "No tiene acceso a esta api"})
     }
 
+    await db.connect()
+    await User.deleteMany()
+    await User.insertMany(initialData.users)
 
+    await db.disconnect()
+
+    res.status(200).json({message: 'Proceso realizado correctamente'})
 }
